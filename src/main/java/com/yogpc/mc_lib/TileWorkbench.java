5825cefc540540f132b0ed754b90bf7d77f3ffdd
yogpstop <github@yogpc.com>
2014-09-10 15:22:43 +0900

    Fix item disappear problem

--- src/main/java/com/yogpc/mc_lib/TileWorkbench.java
+++ src/main/java/com/yogpc/mc_lib/TileWorkbench.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.ChatComponentText;
 
 public class TileWorkbench extends APowerTile {
@@ -31,6 +33,28 @@ public class TileWorkbench extends APowerTile {
       return;
     }
     this.recipe.sendDesc(ep, this);
+  }
+
+  @Override
+  public void readFromNBT(final NBTTagCompound par1NBTTagCompound) {
+    super.readFromNBT(par1NBTTagCompound);
+    final NBTTagList nbttaglist = par1NBTTagCompound.getTagList("Items", 10);
+    for (int i = 0; i < nbttaglist.tagCount(); ++i) {
+      final NBTTagCompound nbttagcompound1 = nbttaglist.getCompoundTagAt(i);
+      this.buffer.add(ItemStack.loadItemStackFromNBT(nbttagcompound1));
+    }
+  }
+
+  @Override
+  public void writeToNBT(final NBTTagCompound par1NBTTagCompound) {
+    super.writeToNBT(par1NBTTagCompound);
+    final NBTTagList nbttaglist = new NBTTagList();
+    for (final ItemStack is : this.buffer) {
+      final NBTTagCompound nbttagcompound1 = new NBTTagCompound();
+      is.writeToNBT(nbttagcompound1);
+      nbttaglist.appendTag(nbttagcompound1);
+    }
+    par1NBTTagCompound.setTag("Items", nbttaglist);
   }
 
   @Override
